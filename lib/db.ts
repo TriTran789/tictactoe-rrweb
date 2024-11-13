bae67930b78895ab8aa601fd0c052bc32534ac77
ThanhTris <thanhtritran8@gmail.com>
2024-11-13 23:55:33 +0700

    add time

--- lib/db.ts
+++ lib/db.ts
@@ -1,48 +1,55 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URI: string | undefined = process.env.MONGO_URI;
 
 if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
 interface MongooseCache {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 declare global {
-    // Allow caching of the connection in the global namespace
-    var mongooseCache: MongooseCache | undefined;
+  // Allow caching of the connection in the global namespace
+  var mongooseCache: MongooseCache | undefined;
 }
 
-const cached: MongooseCache = global.mongooseCache || { conn: null, promise: null };
+const cached: MongooseCache = global.mongooseCache || {
+  conn: null,
+  promise: null,
+};
 
 async function dbConnect(): Promise<Mongoose> {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        };
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
-            console.log('Db connected');
-            return mongoose;
-        });
-    }
-
-    try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
-    }
-
-    global.mongooseCache = cached; // Store connection in global scope
-
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      connectTimeoutMS: 20000, // 20 giây
+      socketTimeoutMS: 45000, // 45 giây
+    };
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      console.log("Db connected");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  global.mongooseCache = cached; // Store connection in global scope
+
+  return cached.conn;
 }
 
 export default dbConnect;
